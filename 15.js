8e058ff9ad87b3ffef6b5b974142ebdee47a1095
Aditya <Adityapandeyadp@gmail.com>
2024-11-12 22:05:47 +0530

    nci

---
+++ 15.js
@@ -0,0 +1,22 @@
+
+var student = {
+        rollno: 6546813,
+        classname: "K24DM",
+        name: "Aditya"
+
+
+}
+console.log(student.name);
+console.log(student);
+var sta1 = new stu(20, 30, 40);
+console.log(sta1)
+function stu(a, b, c) {
+        this.a = a; this.b = b; this.c = c;
+}
+
+
+function good() {
+        console.log("Hello, World!");
+                                     
+}
+good();
